bc189ec055a13612305edbd0a9d9004f84633ed8
Ruben103 <R.D.Schut94@gmail.com>
2020-09-15 14:20:09 +0200

    adding expierments2

--- Assignment2/Experiments.py
+++ Assignment2/Experiments.py
@@ -8,7 +8,7 @@ class Experiments:
         x, y = DataService().read_corpus(trainset)
         x_train, y_train, x_test, y_test = DataService().test_train_split(x,y)
 
-        classifier = ClassifierService().construct_decisiontreeclassifier(criter)
+        classifier = ClassifierService().construct_decisiontreeclassifier()
         classifier.fit(x_train, y_train)
 
         predictions = classifier.predict(x_test)
