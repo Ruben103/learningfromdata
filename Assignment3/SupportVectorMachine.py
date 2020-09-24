ef37bfefa3f50b432cdcbe373cc5af4f5dfe94d2
Ruben103 <R.D.Schut94@gmail.com>
2020-09-24 21:49:07 +0200

    adding stuff

--- Assignment3/SupportVectorMachine.py
+++ Assignment3/SupportVectorMachine.py
@@ -2,9 +2,19 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn import svm
 
-class SupportVectorMachine:
+class SVM:
 
     def construct_classifier(self, kernel):
         if kernel == "linear":
             clf = svm.SVC(kernel="linear")
             return clf
+
+    def vectorize_input(self, x, y):
+        def identity(x):
+            return x
+
+        vec = TfidfVectorizer(preprocessor=identity, tokenizer=identity)
+        x_vec = vec.fit_transform(x)
+        y_vec = vec.fit_transform(y)
+
+        return x_vec.toarray(), y_vec.toarray()
