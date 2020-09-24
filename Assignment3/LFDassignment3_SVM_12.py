8139bf507e5d925dc41319be4ccf7a8e31c09d7d
Sharif <sharifoo.hamed@gmail.com>
2020-09-24 21:26:55 +0200

    error

--- Assignment3/LFDassignment3_SVM_12.py
+++ Assignment3/LFDassignment3_SVM_12.py
@@ -1,14 +1,18 @@
 import sys
+import Experiments
+from pandas import read_csv
 
 from DataService import DataService
 from Experiments import Experiments
 
 
 def main():
-    trainset = sys.argv[1]
-    testset = sys.argv[2]
+    #trainset = sys.argv[1]
+    #testset = sys.argv[2]
 
-    x, y = DataService().read_corpus(trainset)
+    #x, y = DataService().read_corpus(trainset)
+    Experiments().experimentDefaultSetting('trainset.txt', None)
+
 
 
 if __name__ == '__main__':
