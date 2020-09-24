c8589cb994ae8a51b250e87ab2a468492f23d075
Sharif <sharifoo.hamed@gmail.com>
2020-09-24 20:51:40 +0200

    experiment 1

--- Assignment3/Experiments.py
+++ Assignment3/Experiments.py
@@ -1,8 +1,13 @@
 from DataService import DataService
+import SupportVectorMachine
 
 class Experiments:
 
     def experiment1(self, trainset, testset):
         x, y = DataService().read_corpus(trainset)
+        clf = SupportVectorMachine().construct_classifier()
+        x_train, y_train, x_test, y_test = DataService().test_train_split(x, y)
+        clf.fit(x_train, y_train)
+
 
 
